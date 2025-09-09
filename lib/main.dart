afdbce38e5f55fe8ddbcd4708efb9b75c471e9a7
haroon abutabar <HAROONABUTABAR9966@GMAIL.COM>
2025-09-09 14:18:00 +0300

    remove bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ngo/core/helpers/helper.dart';
-import 'package:ngo/core/observer/app_bloc_observer.dart';
 import 'package:ngo/l10n/locale/cubit/locale_cubit.dart';
 import 'package:ngo/ngo.dart';
 import 'package:ngo/service_locator.dart';
@@ -10,7 +8,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SharedPrefHelper.init();
   setupServiceLocator();
-    Bloc.observer = AppBlocObserver();
+    // Bloc.observer = AppBlocObserver();
   LocaleCubit.initial(SharedPrefHelper.instance);
   runApp(const MyApp());
 }
