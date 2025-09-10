a5dbd46ee2a8376087aa8b04e8606bbc6a5b6be1
haroon abutabar <HAROONABUTABAR9966@GMAIL.COM>
2025-09-10 15:53:16 +0300

    Update Android build config and bump app version
    Upgraded compileSdk and targetSdk to 36, updated NDK version, and enhanced packaging, ABI splits, and build options in build.gradle.kts. Added a ProGuard rules file for Flutter and Firebase support. Improved gradle.properties with R8 and NDK build flags for 16KB page size support. Bumped app version to 1.0.0+1 in pubspec.yaml.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,10 +7,34 @@ plugins {
 
 android {
     namespace = "online.lulzie.ngo962.ngo"
-    compileSdk = 35
-    ndkVersion = "28.0.12674087"
+    compileSdk = 36
+    ndkVersion = "29.0.14033849"
 
-    useLegacyPackaging = false
+    packaging {
+        jniLibs {
+            useLegacyPackaging = false  
+            pickFirsts.add("**/libc++_shared.so")
+            pickFirsts.add("**/libjsc.so")
+        }
+        resources {
+            excludes.addAll(listOf(
+                "META-INF/DEPENDENCIES",
+                "META-INF/LICENSE",
+                "META-INF/LICENSE.txt",
+                "META-INF/NOTICE",
+                "META-INF/NOTICE.txt"
+            ))
+        }
+    }
+        
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("arm64-v8a", "armeabi-v7a", "x86_64")
+            isUniversalApk = true
+        }
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -27,16 +51,36 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        vectorDrawables.useSupportLibrary = true
+        multiDexEnabled = true
+
+        ndk {
+            abiFilters += listOf("arm64-v8a", "armeabi-v7a", "x86_64")
+        }
+
+        externalNativeBuild {
+            cmake {
+                cppFlags += "-DPB_FIELD_16BIT"
+                arguments += "-DANDROID_ARM_MODE=arm"
+            }
+        }
     }
 
     buildTypes {
         release {
+            isMinifyEnabled = false
+            isShrinkResources = false
+            // proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+            ndk {
+                debugSymbolLevel = "SYMBOL_TABLE"
+            }
         }
     }
 }
